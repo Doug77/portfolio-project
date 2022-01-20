0bca5ed4e5642c4755fcc1bca8ee3269e43e2fdc
douglas oliveira <douglasgama0@gmail.com>
2022-01-20 12:31:10 -0300

    feat: estilizando footer

--- portfolio/src/components/footer/Footer.jsx
+++ portfolio/src/components/footer/Footer.jsx
@@ -4,7 +4,16 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer>
-      <h3>Um footer</h3>
+      <div className="content-footer">
+        <div>
+          <div>Desenvolvido por Douglas Oliveira</div>
+        </div>
+        <div>|</div>
+        <div>
+          <img src="https://img.icons8.com/material-outlined/48/000000/github.png" alt="github-icon" />
+          <img src="https://img.icons8.com/ios-filled/50/000000/linkedin.png"/>
+        </div>
+      </div>
     </footer>
   );
 }
