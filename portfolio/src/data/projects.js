7ed153aaa351ceba64859b31d76e72cc195fced8
douglas oliveira <douglasgama0@gmail.com>
2022-01-22 14:19:34 -0300

    feat: adicionando informaçoes sobre projetos

---
+++ portfolio/src/data/projects.js
@@ -0,0 +1,56 @@
+export const arrayDataProjects = [
+  {
+    id: 1,
+    project: "Projeto Jest Assíncrono e Mocking",
+    link: "https://github.com/tryber/sd-015-a-project-jest/tree/douglas-oliveira-project-jest",
+    description: ""
+  },
+  {
+    id: 2,
+    project: "Projeto Trybe Wallet",
+    link: "https://github.com/tryber/sd-015-a-project-trybewallet",
+    description: ""
+  },
+  {
+    id: 3,
+    project: "Projeto Front-End Online Store",
+    link: "https://github.com/tryber/sd-015-a-project-frontend-online-store",
+    description: ""
+  },
+  {
+    id: 4,
+    project: "Projeto Front-end Smart Fit",
+    link: "https://github.com/Doug77/desafio-SmartFit",
+    description: ""
+  },
+  {
+    id: 5,
+    project: "Projeto Front-end Original.io",
+    link: "https://github.com/Doug77/original-io",
+    description: ""
+  },
+  {
+    id: 6,
+    project: "Projeto DIO-Clone-Netflix",
+    link: "https://github.com/Doug77/DIO-Clone-Netflix",
+    description: ""
+  },
+  {
+    id: 7,
+    project: "Projeto Portfólio",
+    link: "https://github.com/Doug77/portfolio-project",
+    description: ""
+  },
+  {
+    id: 8,
+    project: "Projeto React Testing Library",
+    link: "https://github.com/tryber/sd-015-a-project-react-testing-library",
+    description: ""
+  },
+  {
+    id: 9,
+    project: "Projeto Tryunfo",
+    link: "https://github.com/tryber/sd-015-a-project-tryunfo",
+    description: ""
+  }
+];
