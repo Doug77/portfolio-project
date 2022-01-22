4877c2ff9f4d4cd8712d5ac6d756213a8fb902eb
douglas oliveira <douglasgama0@gmail.com>
2022-01-22 20:51:22 -0300

    feat: adicionando projetos ao portfolio

--- portfolio/src/components/forMainPage/myProjects/Projects.jsx
+++ portfolio/src/components/forMainPage/myProjects/Projects.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { arrayDataProjects } from '../../../data/projects';
 import './Projects.css';
 
-export default function Projects() {
+import codingProject from '../../../img/icon/codingProject.svg';
 
+export default function Projects() {
   return (
     <div className="project-content" name="projetos">
-      <h1>Projetos que desenvolvi até hoje</h1>
       <div>
-        <div>
-         <h1>projeto 09 fun - jest</h1> 
-         <h1>projeto 06 front - wallet</h1> 
-         <h1>projeto 04 front - ecommerce</h1> 
-        </div>
-        <div>
-          <h1>clone netflix DIO</h1>
-          <h1>original io - ecommerce</h1>
-          <h1>desafio smartfit </h1>
-        </div>
+        <h1 className="header-content-project">Projetos que desenvolvi até hoje 👨‍💻</h1>
+        {
+          arrayDataProjects.map((el, index) => {
+            const {description, project } = el;
+            return (
+              <div className="div-project" key={ index }>
+                <h3 className="title-project">{ project }</h3>
+                <p className="description-project">
+                  { description }
+                </p>
+                <p className="link-to-project"><Link to={{ pathname: "https://github.com/Doug77" }} target="_blank">Link para o repositório</Link></p>
+              </div>
+            )
+          })
+        }
+        <p>Ver todos projetos...</p>
+      </div>
+      <div className="img-content">
+        <img src={ codingProject } alt="icon-developer"  width="700px"/>
       </div>
     </div>
   );
