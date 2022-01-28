fda08634e5380638bc24a7b22ccc81b1aa5fa328
douglas oliveira <douglasgama0@gmail.com>
2022-01-28 13:10:34 -0300

    fix

--- portfolio/src/components/forMainPage/aboutMe/AboutMe.jsx
+++ portfolio/src/components/forMainPage/aboutMe/AboutMe.jsx
@@ -10,28 +10,30 @@ export default function AboutMe() {
   }, [])
 
   return (
-    <div className="about-content" name="sobre">
-      <div className="about-me-name about-div" data-aos="fade-up">
-        <p className="about-my-name about">Sobre mim...</p>
-        <hr className="about-my-name" />
-      </div>
-      <div className="about-div" data-aos="fade-up">
-        <p className="description-me">
-          Sou <b>Desenvolvedor Web<span className="front-end">Front-End</span></b> e estou em busca de me tornar um <b>Desenvolvedor web <span className="full-stack">Full Stack</span></b>.    
-        </p>
-        <p className="description-me">
-          <b>
-          Venho me dedicando a sempre está evoluindo meus conhecimentos e praticando o aprendizado.
-          </b> 
-        </p>
-      </div>
-      <div className="about-div">
-        <img
-          data-aos="fade-up"
-          src="https://avatars.githubusercontent.com/u/83235564?v=4"
-          alt="picture-doug"
-          className="my-photo"
-        />
+    <div className="div-fade">
+      <div className="about-content" name="sobre" data-aos="fade-up">
+        <div className="about-me-name about-div">
+          <p className="about-my-name about">Sobre mim...</p>
+          <hr className="about-my-name" />
+        </div>
+        <div className="about-div">
+          <p className="description-me">
+            Sou <b>Desenvolvedor Web<span className="front-end">Front-End</span></b> e estou em busca de me tornar um <b>Desenvolvedor web <span className="full-stack">Full Stack</span></b>.    
+          </p>
+          <p className="description-me">
+            <b>
+            Venho me dedicando a sempre está evoluindo meus conhecimentos e praticando o aprendizado.
+            </b> 
+          </p>
+        </div>
+        <div className="about-div">
+          <img
+          
+            src="https://avatars.githubusercontent.com/u/83235564?v=4"
+            alt="picture-doug"
+            className="my-photo"
+          />
+        </div>
       </div>
     </div>
   );
